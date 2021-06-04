0c51acb6f5ead304d6f0e9283c8552967d345711
marmooo <marmooo@users.noreply.github.com>
2021-06-04 09:37:29 +0900

    update libraries

--- src/sw.js
+++ src/sw.js
@@ -6,7 +6,7 @@ var urlsToCache = [
   '/type-de-anzan/mp3/end.mp3',
   '/type-de-anzan/mp3/correct3.mp3',
   'https://marmooo.github.io/fonts/textar-light.woff2',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
 ];
 
 self.addEventListener('install', function(event) {
